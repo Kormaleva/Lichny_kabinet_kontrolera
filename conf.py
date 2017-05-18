18b5ab02f1431d19e385a1093b723852975eec61
Kormaleva Elena <lena.kormaleva@gmail.com>
2017-05-18 16:11:17 +0300

    tema

--- conf.py
+++ conf.py
@@ -23,8 +23,7 @@
 
 
 # -- General configuration ------------------------------------------------
-+import sphinx_rtd_theme
-+
+import sphinx_rtd_theme
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
@@ -79,19 +78,15 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
--html_theme = 'alabaster'
- +# html_theme = 'alabaster'
- +html_theme = "sphinx_rtd_theme"
- +
- +html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
- +
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
